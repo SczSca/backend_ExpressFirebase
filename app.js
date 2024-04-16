5af0f5708de9924f4283e3aee5e0ffce9b347f34
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-04-16 07:09:36 -0600

    server first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const {PORT, BASE_URL} = require("./helpers/config");
+
+const express = require('express');
+const routes = require('./routes/index');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors({
+    origin: BASE_URL,
+    credentials: true
+}));
+
+app.use('/api/v0.1', routes); //to use the routes
+app.set('view engine', 'ejs');
+// app.use(express.static('public'));
+// app.use(express.static('helpers'));
+
+const listener = app.listen(PORT || 5000, () => {
+    console.log('Your app is listening on port ' + listener.address().port)
+})
