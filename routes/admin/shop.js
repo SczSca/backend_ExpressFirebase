b7d1454b41f9ebe64389fb539c65c12d3bc20fb1
Antonio Alvarez Mayoral <aalvarez59@ucol.mx>
2024-04-18 08:00:30 -0600

    add middleware that validates jwt

--- routes/admin/shop.js
+++ routes/admin/shop.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
-// const adminMiddleware = require("../middleware/admin.middleware.js");
+const adminMiddleware = require("../../middleware/admin.middleware.js");
 // const userMiddleware = require("../middleware/user.middleware.js");
 // const adminController = require("../controllers/admin.controllers.js");
 const shopController = require("../../controllers/shop.controller");
 
 const router  = express.Router();
 
-router.get('/',shopController.getAllProducts);
-router.post('/product', shopController.newProduct);
+router.get('/', adminMiddleware.isAdminLoggedIn , shopController.getAllProducts);
+//TODO: add middleware
+router.post('/product', adminMiddleware.isAdminLoggedIn , shopController.newProduct);
 
 module.exports = router; 
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const shopController = require("../controllers/shop.controller");
+const userMiddleware = require("../middleware/user.middleware");
 
 const router  = express.Router();
 
 
 //TODO: add middleware
-router.get('/products',shopController.getAllProducts);
-router.post('/buy', shopController.buyProduct);
+router.get('/products', userMiddleware.isLoggedIn , shopController.getAllProducts);
+router.post('/buy', userMiddleware.isLoggedIn , shopController.buyProduct);
 
 module.exports = router;
